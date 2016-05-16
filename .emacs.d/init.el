e1558826bb8dce70bbdf26e5bff8b7bc75d9a60d
Scarlett <gourleys@tcd.ie>
2016-05-16 13:10:33 +0100

    Emacs config for line numbers, indention and themes

---
+++ .emacs.d/init.el
@@ -0,0 +1,66 @@
+;;; -----
+;;; Melpa
+;;; -----
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+    (package-initialize))
+
+
+;;; ------------
+;;; Line Numbers
+;;; ------------
+;; Turn on line numbers
+(global-linum-mode 1)
+
+;; Default string
+(defvar my-linum-format-string "%d \u2502 ")
+
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+	    (lambda ()
+	      (setq-local linum-format-fmt
+			  (let ((w (length (number-to-string
+					    (count-lines (point-min) (point-max))))))
+			    (concat "%" (number-to-string w) "d\u2502 "))))))
+
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-fmt line) 'face 'linum)
+   (propertize "" 'face 'mode-line)))
+
+(unless window-system
+    (setq linum-format 'linum-format-func))
+
+;; Relative line numbers
+(linum-relative-mode 1)
+
+;;; -----------
+;;; Indentation
+;;; -----------
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
+(defvaralias 'c-basic-offset 'tab-width)
+(global-set-key (kbd "DEL") 'backward-delete-char)
+(setq c-backspace-function 'backward-delete-char)
+
+
+;;; -------
+;;; CUSTOM
+;;; -------
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
