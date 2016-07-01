46a05b8ceaa88c5e0dc3a4b3da1e7c01be0fc0d0
Scarlett <gourleys@tcd.ie>
2016-07-01 18:31:35 +0100

    Web mode config

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -1,6 +1,7 @@
-;;; -----
-;;; Melpa
-;;; -----
+;;; -------------
+;;; Bootstrapping
+;;; -------------
+;; Melpa
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -35,13 +36,40 @@
 (setq-default indent-tabs-mode t)
 (setq-default tab-width 4)
 (defvaralias 'c-basic-offset 'tab-width)
-(global-set-key (kbd "DEL") 'backward-delete-char)
+;(global-set-key (kbd "DEL") 'backward-delete-char)
 (setq c-backspace-function 'backward-delete-char)
 
-;; Scala indentation
-(setq scala-indent:step 4)
+;; Python tab with 4 spaces
+(add-hook 'python-mode-hook
+		  (lambda ()
+			(setq indent-tabs-mode t)
+			(setq tab-width 4)
+			(setq python-indent 4)))
 
 
+;;; --------
+;;; Web Mode
+;;; --------
+(require 'web-mode)
+
+;; Auto-load
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; Hooks
+(defun my-web-mode-hook ()
+  ;; Indentation
+  (setq-default indent-tabs-mode nil)
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+		  
 ;;; -------
 ;;; CUSTOM
 ;;; -------
